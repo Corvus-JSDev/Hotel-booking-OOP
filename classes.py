575aead8a245e2fa22536bd2cef5bbe8863fc50f
GiovanniS <giovanni.squillace.work@gmail.com>
2024-09-02 17:08:00 -0400

    refactor

--- classes.py
+++ classes.py
@@ -1,23 +1,40 @@
+from pprint import pp
+import pandas as pd
+
 class User:
 	pass
 
 
 class Hotel:
-	def __init__(self, hotel_data, hotel_id=134):
-		self.hotel_data = hotel_data
-		self.hotel_id = hotel_id
+	def __init__(self, customer_name="Customer"):
+		self.customer_name = customer_name.lower()
+		self.df = pd.read_csv("hotel_data.csv")
 
 
-	def book_room(self, name_of_user):
-		pass
+	def book_room(self, hotel_id):
+		# Reassign the value to "no" to show it has been booked an is full
+		df = self.df
+		df.loc[df["id"] == hotel_id, "available"] = "no"
+
+		# Save the changes
+		df.to_csv("hotel_data.csv", index=False)  # Set index=False so python doesn't add another index
+
+		print("Your room has been booked")
 
 
 	def list_hotels(self):
-		pass
+		pp(self.df)
 
 
-	def is_available(self):
-		print(self.hotel_data[self.hotel_id])
+	def is_available(self, hotel_id):
+		try:
+			df = self.df
+			row = df.loc[df["id"] == hotel_id]
+			available = row["available"].squeeze()
+
+			return True if available == "yes" else False
+		except ValueError:
+			return False
 
 
 class Reservation:
@@ -29,3 +46,22 @@ class Reservation:
 	def generate_invoice(self):
 		content = f"{self.customer_name} has booked a room at {self.hotel_obj}"
 		return content
+
+
+if __name__ == "__main__":
+	hotel = Hotel("Gio")
+	hotel.list_hotels()
+
+	hotel_id = int(input("\nEnter the ID of the hotel: "))
+
+	# Booking a room
+	if hotel.is_available(hotel_id):
+		hotel.book_room(hotel_id)
+
+		"""
+		res = Reservation("hotel_test", "Giovanni")
+		print(res.generate_invoice())
+		"""
+
+	else:
+		print("This hotel is unavailable")
--- main.py
+++ main.py
@@ -2,38 +2,20 @@ from classes import *
 import pandas as pd
 from pprint import pp
 
-hotel_data = pd.read_csv("hotel_data.csv")
-
 help_commands = """
 list - Show a list of all hotels.
 quit - End the program."""
 
-# print("Welcome to hotel booking!\n")
-
-# Booking a room
-hotel = Hotel(hotel_data)
-"""
-if hotel.is_available():
-	hotel.book_room("Gio")
-
-	res = Reservation("hotel_test", "Giovanni")
-	print(res.generate_invoice())
-
-else:
-	print("This hotel is unavailable")
-"""
-
-
-
+print("Welcome to hotel booking!\n")
 
 while True:
-	user_choice = input("\nWhat would you like to do: ").lower().strip()
-
+	# user_choice = input("\nWhat would you like to do: ").lower().strip()
+	user_choice = "q"
 
 	match user_choice:
 		case "l" | "list":
 			print(" ")
-			print(hotel_data)
+			print(hotel.list_hotels())
 
 		case "h" | "help":
 			print(help_commands)
