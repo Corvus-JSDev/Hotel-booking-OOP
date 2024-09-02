724179a4cfba74c7908b0ab77f1bad4aed1dcf71
GiovanniS <giovanni.squillace.work@gmail.com>
2024-09-02 16:23:09 -0400

    refactor

--- classes.py
+++ classes.py
@@ -3,6 +3,11 @@ class User:
 
 
 class Hotel:
+	def __init__(self, hotel_data, hotel_id=134):
+		self.hotel_data = hotel_data
+		self.hotel_id = hotel_id
+
+
 	def book_room(self, name_of_user):
 		pass
 
@@ -11,6 +16,16 @@ class Hotel:
 		pass
 
 
+	def is_available(self):
+		print(self.hotel_data[self.hotel_id])
+
+
 class Reservation:
-	def generate(self):
-		pass
+	def __init__(self, hotel_obj, customer_name):
+		self.hotel_obj = hotel_obj.lower()
+		self.customer_name = customer_name.lower()
+
+
+	def generate_invoice(self):
+		content = f"{self.customer_name} has booked a room at {self.hotel_obj}"
+		return content
